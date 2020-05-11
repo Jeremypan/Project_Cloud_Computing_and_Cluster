993a00c406ef186ebfce60deb78226c94c471fac
Hangyu Pan <phyjeremy@outlook.com>
2020-05-11 15:41:01 +1000

    Finish Couchdb Cluster

--- ansible/run_ansible_launch_nectar.sh
+++ ansible/run_ansible_launch_nectar.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-. ./openrc.sh; ansible-playbook -i hosts --ask-become-pass run_nectar.yaml
+. ./openrc.sh; ansible-playbook -i hosts --ask-become-pass run_nectar.yaml -e 'ansible_python_interpreter=/usr/bin/python3'
