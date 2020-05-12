0165abcdb452ba0592d4a2df02cd9f8183340595
Hangyu Pan <phyjeremy@outlook.com>
2020-05-12 17:57:47 +1000

    fix ansible_bash

--- ansible/run_ansible_launch_nectar.sh
+++ ansible/run_ansible_launch_nectar.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-. ./openrc.sh; ansible-playbook -i hosts --ask-become-pass run_nectar.yaml -e 'ansible_python_interpreter=/usr/bin/python3'
+. ./openrc.sh; ansible-playbook -i hosts --ask-become-pass run_nectar.yaml -e'ansible_python_interpreter=/usr/bin/python3'
