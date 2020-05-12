6ce07cd47234d2c193a4d8106baed81cbb51f62b
Hangyu Pan <phyjeremy@outlook.com>
2020-05-12 21:43:13 +1000

    debug template

---
+++ ansible/roles/harvester_deployment/templates/harvestor/auth.py
@@ -0,0 +1,4 @@
+consumer_key= "{{ item.consumer_key }}"
+consumer_secret="{{ item.consumer_secret }}"
+access_token="{{ item.access_token }}"
+access_token_secret="{{ item.consumer_key }}"
--- ansible/run_ansible_launch_nectar.sh
+++ ansible/run_ansible_launch_nectar.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-
 . ./openrc.sh; ansible-playbook -i hosts --ask-become-pass run_nectar.yaml -e'ansible_python_interpreter=/usr/bin/python3'
